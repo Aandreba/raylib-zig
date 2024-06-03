3a3699c443facdd60c5f0152b60a1a6e63f9f4f2
Aandreba <aandrebafreelancer@gmail.com>
2024-06-03 22:52:28 +0200

    fixes

--- lib/raygui.h
+++ lib/raygui.h
@@ -2594,7 +2594,7 @@ int GuiButton(Rectangle bounds, const char *text) {
     // Check button state
     if (CheckCollisionPointRec(mousePoint, bounds)) {
       result = IsMouseButtonDown(MOUSE_LEFT_BUTTON);
-      RAYGUI_LOG("%i", result);
+      printf("%i", result);
 
       if (result)
         state = STATE_PRESSED;
